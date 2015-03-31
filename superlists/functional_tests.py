8b63b88f43c90b6a2f44f658eccb129e99aa44dd
Leandro Barbosa <leandro@tree.sh>
2015-03-31 17:39:13 -0300

    Add new class to the tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,28 @@
 #coding: utf-8
+from unittest import TestCase
 from selenium import webdriver
 
 browser = webdriver.Firefox()
 browser.get('http://localhost:9000')
 
-assert  'Django' in browser.title
+assert  'To-Do' in browser.title, "Browser Title was " + browser.title
+
+browser.quit()
+
+
+class NewVisitorTest(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retriever_it_later(self):
+        self.browser.get('http://localhost:9000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish Test')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
